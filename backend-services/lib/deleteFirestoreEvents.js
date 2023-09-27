3a75311d5fff912e1994627e870e97de5220e2b9
bebok <bebok>
2023-09-27 16:36:11 +0200

    change

---
+++ backend-services/lib/deleteFirestoreEvents.js
@@ -0,0 +1,38 @@
+async function deleteCollection(db, collectionPath, batchSize) {
+    const collectionRef = db.collection(collectionPath);
+    const query = collectionRef.orderBy('__name__').limit(batchSize);
+
+    return new Promise((resolve, reject) => {
+        deleteQueryBatch(db, query, resolve).catch(reject);
+    });
+}
+
+async function deleteQueryBatch(db, query, resolve) {
+    const snapshot = await query.get();
+
+    const batchSize = snapshot.size;
+    if (batchSize === 0) {
+        resolve();
+        return;
+    }
+
+    const batch = db.batch();
+    snapshot.docs.forEach((doc) => {
+        batch.delete(doc.ref);
+    });
+    await batch.commit();
+
+    // Recurse on the next process tick, to avoid
+    // exploding the stack.
+    process.nextTick(() => {
+        deleteQueryBatch(db, query, resolve);
+    });
+}
+
+module.exports ={
+    deleteFirestoreEvents: (async (firestore) => {
+        const result = await deleteCollection(firestore, 'events', 100)
+        console.log('Events deleted')
+        return result;
+    })
+}
---
+++ backend-services/start-scraper.js
@@ -0,0 +1,48 @@
+const eventSources = require('../data/eventSources.json');
+const {pipe, pluck, map, take, andThen, otherwise, flatten, tap} = require("ramda");
+const {fetchFBEventsForVenue} = require("./lib/fetchFacebookEvents");
+const {Firestore} = require("@google-cloud/firestore");
+const Promise = require('bluebird')
+const {deleteFirestoreEvents} = require("./lib/deleteFirestoreEvents");
+
+
+const firestore = new Firestore({
+    keyFilename: '../service-key.json',
+});
+
+const convertToPlainObject = v => ({...v});
+
+deleteFirestoreEvents(firestore).then(() => pipe(
+    pluck('url'),
+    take(5), // just for testing
+    v => Promise.mapSeries(v, fetchFBEventsForVenue),
+    tap(andThen(v => {
+        console.log('DODODODODOD before concat length', v.length);
+
+        v.map((v, index) => {
+                console.log(index, 'DODODODODOD after series length', v.length);
+        },v)
+    })),
+    v => Promise.all(v),
+    andThen(flatten),
+    tap(andThen(v => {
+        console.log('DODODODODOD after series length', v.length);
+    })),
+    andThen((items)  => {
+
+        const collection = firestore.collection('events')
+
+        console.log('DODODODODOD all events length', items.length)
+
+        return map(pipe(
+            convertToPlainObject,
+            v => collection.add(v),
+        ))(items)
+    }),
+    v => Promise.all(v),
+    otherwise((err) => console.log(err)),
+    andThen(() => {
+        console.log('Data scraped and saved!')
+    })
+)(eventSources))
+
