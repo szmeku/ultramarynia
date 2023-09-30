6742ca3ac5e07316a35e06890c5f562c186a0e5f
bebok <bebok>
2023-09-30 14:52:41 +0200

    change

--- backend-services/start-scraper.js
+++ backend-services/start-scraper.js
@@ -1,5 +1,5 @@
 const eventSources = require('../data/eventSources.json');
-const {pipe, pluck, map, take, andThen, otherwise, flatten, tap, uniqBy} = require("ramda");
+const {pipe, pluck, map, take, andThen, otherwise, flatten, tap, uniqBy, equals} = require("ramda");
 const {fetchFBEventsForVenue} = require("./lib/fetchFacebookEvents");
 const {Firestore} = require("@google-cloud/firestore");
 const Promise = require('bluebird')
@@ -10,9 +10,30 @@ const firestore = new Firestore({
     keyFilename: '../service-key.json',
 });
 
+const repeatUntilTrue = (fn) => fn()
+    .then(v => {
+        if (v === true) {
+            console.log('success')
+            return v;
+        } else {
+            console.log('failed, trying again');
+            return repeatUntilTrue(fn)
+        }
+    })
+
+
+const testWebsite = () => {
+    return fetch('https://katokult.pl').then(v => v.text()).then(v => {
+
+        console.log('v', v)
+
+        return v.includes('Poniedziałek')
+    })
+}
+
 const convertToPlainObject = v => ({...v});
 
-deleteFirestoreEvents(firestore).then(() => pipe(
+pipe(
     pluck('url'),
     // take(1), // just for testing
     v => Promise.mapSeries(v, fetchFBEventsForVenue),
@@ -20,8 +41,8 @@ deleteFirestoreEvents(firestore).then(() => pipe(
         console.log('DODODODODOD before concat length', v.length);
 
         v.map((v, index) => {
-                console.log(index, 'DODODODODOD after series length', v.length);
-        },v)
+            console.log(index, 'DODODODODOD after series length', v.length);
+        }, v)
     })),
     v => Promise.all(v),
     andThen(flatten),
@@ -29,27 +50,36 @@ deleteFirestoreEvents(firestore).then(() => pipe(
         console.log('DODODODODOD after series length', v.length);
     })),
     andThen(uniqBy(v => `${v.datePl}||${v.hourPl}||${v.venue}||${v.title}`)),
-    andThen((items)  => {
+    andThen((items) => {
 
-        const collection = firestore.collection('events')
+        if (items.length > 0) {
+            return deleteFirestoreEvents(firestore).then(() => {
+                const collection = firestore.collection('events');
 
-        console.log('DODODODODOD all events length', items.length)
+                console.log('DODODODODOD all events length', items.length)
 
-        return map(pipe(
-            convertToPlainObject,
-            v => collection.add(v),
-        ))(items)
+                return map(pipe(
+                    convertToPlainObject,
+                    v => collection.add(v),
+                ))(items)
+            })
+        } else {
+            throw Error("didn't downloaded any events")
+        }
     }),
     v => Promise.all(v),
     andThen(() => {
         console.log('Data scraped and saved!')
-        return fetch(netlifyBuildHook, {method: 'POST'}).then(() => console.log('Netlify build hook fired!'));
+
+        return repeatUntilTrue(
+            () => fetch(netlifyBuildHook, {method: 'POST'})
+                .then(() => Promise.delay(120000))
+                .then(() => testWebsite())
+        )
     }),
     andThen(() => process.exit()),
     otherwise((err) => {
         console.log(err)
         process.exit()
     })
-
-)(eventSources))
-
+)(eventSources)
