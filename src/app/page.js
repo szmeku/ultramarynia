1f5f3098554ad18b963ded887b979fe5a974f331
bebok <bebok>
2024-07-04 17:10:44 +0200

    change

--- src/app/page.js
+++ src/app/page.js
@@ -1,57 +1,52 @@
-import {fetchEventsGroupedByDates} from "@/services/events";
-import {pipe, values} from "ramda";
+import { fetchEventsGroupedByDates } from "@/services/events";
+import { pipe, values } from "ramda";
 import './styles.css';
 const moment = require('moment');
 
 const dateToGoogleCalendarDate = (rawDate) => {
-
     const date = moment(rawDate);
     const endDate = date.clone().add(1, 'hours');
-
     const formattedStartDate = date.utc(true).format("YYYYMMDDTHHmmss");
     const formattedEndDate = endDate.utc(true).format("YYYYMMDDTHHmmss");
-
     return `${formattedStartDate}/${formattedEndDate}`;
 }
 
-const Event = ({event}) => {
+const Event = ({ event }) => {
+    const addToCalendarUrl = `https://www.google.com/calendar/render?action=TEMPLATE&text=${event.title}&dates=${dateToGoogleCalendarDate(event.dateAndTime)}&details=${event.url}`;
 
-    const addToCalendarUrl = `https://www.google.com/calendar/render?action=TEMPLATE&text=${event.title}&dates=${dateToGoogleCalendarDate(event.dateAndTime)}&details=${event.url}`
-    // todo: add location at some point
-    //  &location=Location
-
-    return <li key={event.title} style={{padding: 5, marginBottom: 8}}>
-        <span className="hourChip">{event.hourPl}</span>&nbsp;&nbsp;
-        <a className={"save"} href={addToCalendarUrl} target="_blank"><img src={'save.png'} alt={'save'}/></a>&nbsp;&nbsp;
-        <a href={event.url} target="_blank">{event.title}</a>&nbsp;&nbsp;
-        <span className="lighter">{event.venue}</span>&nbsp;&nbsp;
-        <span className="lighter">{event.category}</span>
-    </li>
+    return (
+        <li key={event.title}>
+            <div className="hourChip">
+                <a target="_blank" href={addToCalendarUrl}>
+                <img src={'save.png'} alt={'calendar icon'} /></a>
+                &nbsp;{event.hourPl}
+            </div>
+            <a className="event-title" href={event.url} target="_blank">{event.title}</a>
+            <div className="venue-category">
+                {event.venue}&nbsp;&nbsp;|&nbsp;&nbsp;{event.category}
+            </div>
+        </li>
+    );
 }
 
-const EventsGroup = ({events, light}) => {
-
-    const backgroundColor = light ? '#f2f2f2' : '#e6e6e6';
-
-    return <div className="eventsGroup" style={{backgroundColor}}>
-        <span style={{fontSize: 29, weight: 'bold', marginBottom: 15, display: 'inline-block',  textTransform: 'uppercase'}}>{events[0].dayOfTheWeekPl}</span>
-        <span className="date" style={{marginLeft: 10}}>{events[0].datePl}</span>
-        <ul style={{listStyleType: 'none', margin: 0, padding: 0, paddingTop: 15, borderTop: '3px solid black'}}>
-            {events.map(event => <Event key={event.title} event={event}/>)}
-        </ul>
-    </div>
-
-
+const EventsGroup = ({ events }) => {
+    return (
+        <div className="eventsGroup">
+            <span style={{ fontSize: '1.8em', fontWeight: 'bold' }}>{events[0].dayOfTheWeekPl}</span>
+            <span className="date">{events[0].datePl}</span>
+            <ul>
+                {events.map(event => <Event key={event.title} event={event} />)}
+            </ul>
+        </div>
+    );
 }
 
 export default async function Home() {
-
-    const eventsGroupedByDate = await fetchEventsGroupedByDates()
+    const eventsGroupedByDate = await fetchEventsGroupedByDates();
 
     return (
-        <div className={"main"}>
+        <div className="main">
             <div className="top-info">
-                {/*<img src="logo.png" alt="Kato Kult"/>*/}
                 <h1>Kato Kult</h1>
                 <div className="disclaimer">
                     <p>Agregator wydarzeń kulturalnych w Katowicach.</p>
@@ -60,8 +55,8 @@ export default async function Home() {
             </div>
             {pipe(
                 values,
-                v => v.map((events, index) =>  <EventsGroup key={index} events={events} light={!!(index % 2)}/>),
+                v => v.map((events, index) => <EventsGroup key={index} events={events} />),
             )(eventsGroupedByDate)}
         </div>
     );
-};
+}
