af83c5d52f6bed3d2b8d2b0dc173e61084643165
bebok <random@mail>
2025-10-30 15:31:41 +0100

    change

--- backend-services/lib/extractEventsFromStrings.js
+++ backend-services/lib/extractEventsFromStrings.js
@@ -26,7 +26,8 @@ module.exports = {
                     role: "user",
                     "content": `From following strings extract events and run proper callback function.
                     Please remember that dateAndTime of the event should be in this format 'YYYY-MM-DDTHH:mm:ss'.
-                    Assume that today is ${moment().format('YYYY-MM-DD, dddd')}, this is really important for extracting date. If you notice duplicated events (same venue, time, date and super similar name) remove them (of course leave one).
+                    Assume that today is ${moment().format('YYYY-MM-DD, dddd')}, this is really important for extracting date.
+                    IMPORTANT: Don't miss any event! Unless there's a duplicate (check by event url) if yes skip duplicate, we want just one occurrence of each event.
                     Also for every event decide about a category, one from ['music', 'film', 'health', 'art', 'theater', 'other'] \n\n ${v}`
                 },
             ],
